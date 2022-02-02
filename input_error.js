46f53de1a8142f794be7703bdfcb345c345fc36d
anamerasul <anamerasul@gmail.com>
2022-02-02 17:20:18 +0600

    input error

---
+++ input_error.js
@@ -0,0 +1,30 @@
+function add(num1, num2){
+               const sum =num1+num2;
+               return sum;
+
+
+}
+
+// const firstTotal =add(63,7);
+// console.log(firstTotal);
+
+// const secondTotal =add(54, 982);
+
+// console.log(secondTotal);
+const thirdTotal =add(54,982);
+
+console.log(thirdTotal);
+
+
+function multiply(num1, num2){
+
+                    console.log("parameter",num1,num2)
+                    const result =num1*num2;
+
+                    return result;
+
+}
+
+
+const firstResult = multiply(13,10);
+console.log(firstResult);
